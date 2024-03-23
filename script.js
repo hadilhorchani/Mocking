dc33cab810cf1cbdb1539d122a87b4535169f972
hadilhorchani <159797266+hadilhorchani@users.noreply.github.com>
2024-03-23 18:04:09 +0100

    Create script.js
    This the original code of my project opinion exchange written in js ready for mocking

---
+++ script.js
@@ -0,0 +1,27 @@
+// Define a class for the Opinion Exchange
+class OpinionExchange {
+    constructor() {
+        this.objects = {};
+    }
+
+    // Method to add object to chat about 
+    addObject(objectName) {
+        if (!this.objects[objectName]) {
+            this.objects[objectName] = [];
+            console.log(`"${objectName}" added to the Opinion Exchange.`);
+            showNotification(`"${objectName}" added to the Opinion Exchange.`, 'success');
+        } else {
+            console.log(`"${objectName}" already exists in the Opinion Exchange.`);
+            showNotification(`"${objectName}" already exists in the Opinion Exchange.`, 'error');
+        }
+    }
+
+    // Method to add opinion
+    addOpinion(objectName, opinion) {
+        if (this.objects[objectName]) {
+            this.objects[objectName].push(opinion);
+            console.log(`Opinion added for "${objectName}": "${opinion}"`);
+            showNotification(`Opinion added for "${objectName}": "${opinion}"`, 'success');
+        } else {
+            console.log(`"${objectName}" does not exist in the Opinion Exchange.`);
+            showNotification(`"${objectName}" does not exist in the Opinion Exchange.`, 'error');
